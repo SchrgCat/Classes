f738a5d7355775d7bcca2dc752bf81df0c182b16
Mikołaj Skawiński <>
2018-09-06 16:37:09 +0200

    initialize project

---
+++ Classes/Classes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Classes
+//
+//  Created by Mikołaj Skawiński on 06.09.2018.
+//  Copyright © 2018 Mikołaj Skawiński. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
